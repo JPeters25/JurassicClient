62aa5c1fb75c9997a310ce39438f66d2c3b7bde2
Justin Peters <JMarvel525@gmail.com>
2022-08-17 12:02:48 -0700

    initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace JurassicClient.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
